37ec01167ea506bd39843f2f4da1c6dde6427e39
MilanCiprian <smartgest@yahoo.com>
2024-11-07 00:30:09 +0200

    error

--- src/app/_util/getIP.ts
+++ src/app/_util/getIP.ts
@@ -2,7 +2,7 @@
 import { headers } from "next/headers";
 
 export async function getIP() {
-  const requestHeaders = await headers(); // this generate the error
+  const requestHeaders = await headers(); // this line generate the error
   const remoteIP = requestHeaders.get("x-forwarded-for")?.split(":").pop() || "unknown";
   return remoteIP;
 }
